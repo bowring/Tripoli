ac2d7efb1bcffa09644d1cef739c62f32350cb0c
Bowring <bowring@gmail.com>
2021-04-13 13:18:50 -0400

    Updated dates in TIMSDP files

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -45,5 +45,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // Aug 2011 starting with 4.6.4.00, we are using Visual Studio 2010 and dotNet 3.5
-[assembly: AssemblyVersion("4.10.0.0")]
-[assembly: AssemblyFileVersion("4.10.0.0")]
+[assembly: AssemblyVersion("4.10.0.1")]
+[assembly: AssemblyFileVersion("4.10.0.1")]
